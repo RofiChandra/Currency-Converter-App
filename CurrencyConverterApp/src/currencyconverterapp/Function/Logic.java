53f46da6a0b7c18af8a12ecb88df9d3fdd870884
christianfrans <61776387+christianfrans@users.noreply.github.com>
2020-03-19 18:39:48 +0700

    Add files via upload

---
+++ CurrencyConverterApp/src/currencyconverterapp/Function/Logic.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package currencyconverterapp.Function;
+
+import currencyconverterapp.Model.ModelProgram;
+
+/**
+ *
+ * @author DEYA, ROFI, CHRISTIAN
+ */
+public class Logic {
+    float returnRupiah(ModelProgram modelProgram){
+        return modelProgram.getRupiah();
+    }
+    
+    float returnDollar(ModelProgram modelProgram){
+        return modelProgram.getDollar();
+    }
+    
+    float returnEuro(ModelProgram modelProgram){
+        return modelProgram.getEuro();
+    }
+    
+    float returnYen(ModelProgram modelProgram){
+        return modelProgram.getYen();
+    }
+}
---
+++ CurrencyConverterApp/src/currencyconverterapp/Model/ModelProgram.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package currencyconverterapp.Model;
+
+/**
+ *
+ * @author DEYA, ROFI, CHRISTIAN
+ */
+public class ModelProgram {
+
+    float rupiah, dollar, euro, yen;
+
+    public void setRupiah(float rupiah) {
+        this.rupiah = rupiah;
+    }
+
+    public float getRupiah() {
+        return rupiah;
+    }
+
+    public void setDollar(float dollar) {
+        this.dollar = dollar;
+    }
+
+    public float getDollar() {
+        return dollar;
+    }
+
+    public void setEuro(float euro) {
+        this.euro = euro;
+    }
+
+    public float getEuro() {
+        return euro;
+    }
+
+    public void setYen(float yen) {
+        this.yen = yen;
+    }
+
+    public float getYen() {
+        return yen;
+    }
+
+}
